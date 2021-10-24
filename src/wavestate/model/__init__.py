0ba0d50c57acae4cae5db1c48692f6521593f284
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-23 23:58:36 -0500

    add versioning code

--- src/wavestate/model/__init__.py
+++ src/wavestate/model/__init__.py
@@ -7,6 +7,15 @@
 # with details inline in source files, comments, and docstrings.
 """
 """
-
-
+from ._version import version, __version__, version_info
 from .optics1064 import system1064, system1550
+
+
+__all__ = [
+    'version',
+    '__version__',
+    'version_info',
+    'system1064',
+    'system1550',
+]
+
